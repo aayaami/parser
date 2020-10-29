fc7876e6d491199ff5eb218ff0879bed817bc2f0
aayaami <vostroknutov.alexander@gmail.com>
2020-10-29 12:58:41 +0400

    feat: Парсер сайта https://lgototvet.ru

---
+++ components/parser/news/LgotOtverRuParser.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace app\components\parser\news;
+
+use app\components\helper\nai4rus\AbstractBaseParser;
+use app\components\helper\nai4rus\NewsPostItemDTO;
+use app\components\helper\nai4rus\PreviewNewsDTO;
+use app\components\parser\NewsPost;
+use DateTimeImmutable;
+use DateTimeZone;
+use DOMNode;
+use RuntimeException;
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\DomCrawler\UriResolver;
+use Throwable;
+
+class LgotOtverRuParser extends AbstractBaseParser
+{
+    public const USER_ID = 2;
+    public const FEED_ID = 2;
+
+    protected function getSiteUrl(): string
+    {
+        return 'https://lgototvet.ru';
+    }
+
+    protected function searchQuoteNewsItem(DOMNode $node): ?NewsPostItemDTO
+    {
+        try {
+            if (method_exists($node, 'getAttribute') && $node->getAttribute('class') === 'article__quote-marks') {
+                $node = (new Crawler($node->parentNode))->filterXPath('//div[@class="article__quote-body"]')->getNode(0);
+                $newsPostItem = NewsPostItemDTO::createQuoteItem($this->normalizeText($node->textContent));
+                $this->getNodeStorage()->attach($node, $newsPostItem);
+                $this->removeParentsFromStorage($node->parentNode);
+                return $newsPostItem;
+            }
+        } catch (\Throwable $th) {
+        }
+
+        if ($node->nodeName === '#text' || !$this->isQuoteType($node)) {
+            $parentNode = $this->getRecursivelyParentNode($node, function (DOMNode $parentNode) {
+                $isQuote = $this->isQuoteType($parentNode);
+
+                if ($this->getRootContentNodeStorage()->contains($parentNode) && !$isQuote) {
+                    return null;
+                }
+
+                return $isQuote;
+            });
+            $node = $parentNode ?: $node;
+        }
+
+        if (!$this->isQuoteType($node) || !$this->hasText($node)) {
+            return null;
+        }
+
+        if ($this->getNodeStorage()->contains($node)) {
+            throw new RuntimeException('Тег уже сохранен');
+        }
+
+        $newsPostItem = NewsPostItemDTO::createQuoteItem($this->normalizeText($node->textContent));
+
+        $this->getNodeStorage()->attach($node, $newsPostItem);
+        $this->removeParentsFromStorage($node->parentNode);
+
+        return $newsPostItem;
+    }
+
+    protected function getPreviewNewsDTOList(int $minNewsCount = 10, int $maxNewsCount = 100): array
+    {
+        $previewList = [];
+        $url = "/feed/zen";
+        $uriPreviewPage = UriResolver::resolve($url, $this->getSiteUrl());
+
+        try {
+            $previewNewsContent = $this->getPageContent($uriPreviewPage);
+            $previewNewsCrawler = new Crawler($previewNewsContent);
+        } catch (Throwable $exception) {
+            throw new RuntimeException('Не удалось получить достаточное кол-во новостей', null, $exception);
+        }
+
+        $previewNewsCrawler = $previewNewsCrawler->filterXPath('//item');
+        if ($previewNewsCrawler->count() < $minNewsCount) {
+            throw new RuntimeException('Не удалось получить достаточное кол-во новостей');
+        }
+
+        $previewNewsCrawler->each(function (Crawler $newsPreview) use (&$previewList, $url) {
+            $title = $newsPreview->filterXPath('//title')->text();
+            $uri = $newsPreview->filterXPath('//link')->text();
+            $publishedAtString = $newsPreview->filterXPath('//pubDate')->text();
+            $preview = null;
+
+            $publishedAt = DateTimeImmutable::createFromFormat('D, d M Y H:i:s O', $publishedAtString);
+            $publishedAtUTC = $publishedAt->setTimezone(new DateTimeZone('UTC'));
+
+            $previewList[] = new PreviewNewsDTO($uri, $publishedAtUTC, $title, $preview);
+        });
+
+        if (count($previewList) < $minNewsCount) {
+            throw new RuntimeException('Не удалось получить достаточное кол-во новостей');
+        }
+
+        $previewList = array_slice($previewList, 0, $maxNewsCount);
+
+        return $previewList;
+    }
+
+    protected function parseNewsPage(PreviewNewsDTO $previewNewsItem): NewsPost
+    {
+        $uri = $previewNewsItem->getUri();
+
+        $newsPage = $this->getPageContent($uri);
+
+        $newsPageCrawler = new Crawler($newsPage);
+        $newsPostCrawler = $newsPageCrawler->filterXPath('//article');
+
+        $image = null;
+        $mainImageCrawler = $newsPageCrawler->filterXPath('//meta[@property="og:image"]')->first();
+        if ($this->crawlerHasNodes($mainImageCrawler)) {
+            $image = $mainImageCrawler->attr('content');
+        }
+        if ($image !== null && $image !== '') {
+            $previewNewsItem->setImage(UriResolver::resolve($image, $uri));
+        }
+
+        $descriptionCrawler = $newsPostCrawler->filterXPath('//h5[1]');
+        if ($this->crawlerHasNodes($descriptionCrawler) && $descriptionCrawler->text() !== '') {
+            $previewNewsItem->setDescription($descriptionCrawler->text());
+            $this->removeDomNodes($newsPostCrawler, '//h5[1]');
+        }
+
+        $contentCrawler = $newsPostCrawler->filterXPath('//div[@itemprop="articleBody"]');
+
+        $this->removeDomNodes($contentCrawler, '//*[contains(translate(substring(text(), 0, 14), "ФОТО", "фото"), "фото")]
+        | //div[child::a[@class="article__post-link"]]');
+
+        $this->purifyNewsPostContent($contentCrawler);
+
+        $newsPostItemDTOList = $this->parseNewsPostContent($contentCrawler, $previewNewsItem);
+
+        return $this->factoryNewsPost($previewNewsItem, $newsPostItemDTOList);
+    }
+}
